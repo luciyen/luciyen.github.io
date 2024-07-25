1762d30d06949c3b19f09daf52e7666f2185843b
jahnltib <jahnltib+github@gmail.com>
2024-07-25 01:44:31 -0700

    added GA

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+
+// Function to load Google Analytics script
+const loadGoogleAnalytics = () => {
+  const script = document.createElement('script');
+  script.src = `https://www.googletagmanager.com/gtag/js?id=G-1WPX9D372W`;
+  script.async = true;
+  document.head.appendChild(script);
+
+  script.onload = () => {
+    window.dataLayer = window.dataLayer || [];
+    function gtag() { dataLayer.push(arguments); }
+    gtag('js', new Date());
+    gtag('config', 'G-1WPX9D372W');
+  };
+};
+
+// Load Google Analytics script
+loadGoogleAnalytics();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+);
